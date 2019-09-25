bc35f7ed01b7edac3ca7b80b5bfeb415b7fbacfa
lucaono133 <italia.soccer7@gmail.com>
2019-09-25 15:19:46 -0700

    Quick Updates

---
+++ Soccer_Project/Transfers/teams.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import sys
+sys.path.append('home/lucaono13/Documents/GitHub_Projects/APIs/Soccer/Soccer_Project/')
+import clean_standings
+#team_ids = pd.read_csv('/home/lucaono13/Documents/GitHub_Projects/APIs/Soccer/Soccer_Project/Cleaned_Up_Standings/Serie A_standings.csv')
+print(team_ids)
