02fe703e2cf579b8dc8f4b49ffea01d03942be34
lucaono133 <italia.soccer7@gmail.com>
2019-09-25 09:15:52 -0700

    Created bat file for 1 time a season set up of grabbing all league IDs

---
+++ Soccer_Project/setup_league_id.bat
@@ -0,0 +1,2 @@
+start /wait  /home/lucaono13/Documents/GitHub_Projects/APIs/Soccer/Soccer_Project/leagues_req.py
+start /wait /home/lucaono13/Documents/GitHub_Projects/APIs/Soccer/Soccer_Project/usage_league_id.py
