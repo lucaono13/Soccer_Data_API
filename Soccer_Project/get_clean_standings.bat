7a47b86c010b3f53354df4c285c5780bfedc6c59
lucaono133 <italia.soccer7@gmail.com>
2019-09-25 09:18:30 -0700

    Bat file to get standings of certain leagues and output clean version

---
+++ Soccer_Project/get_clean_standings.bat
@@ -0,0 +1,4 @@
+start /wait /home/lucaono13/Documents/GitHub_Projects/APIs/Soccer/Soccer_Project/dict_create.py
+start /wait /home/lucaono13/Documents/GitHub_Projects/APIs/Soccer/Soccer_Project/get_league_standing.py
+start /wait /home/lucaono13/Documents/GitHub_Projects/APIs/Soccer/Soccer_Project/get_standing.py
+start /wait /home/lucaono13/Documents/GitHub_Projects/APIs/Soccer/Soccer_Project/clean_standings.py
